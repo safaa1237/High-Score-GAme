01c595cf7d40104fa4118c8e43366b40566df624
Safaa el ayoubi <41582021+safaa1237@users.noreply.github.com>
2021-03-25 16:51:38 +0100

    Delete cli directory

--- cli/CmdLineParser.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <string>
-
-// Display the usage details with the command line help
-void displayHelp(const std::wstring& exePath);
-
-// Parses the CLI options/arguments and returns them as struct
-void parseCliOptions(int argc, wchar_t** argv, bool& teamCity, std::wstring& workingDir, std::wstring& inputDir, std::wstring& outputDir);
